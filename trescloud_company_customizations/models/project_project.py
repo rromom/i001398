4a5f9f6b81a2618686549615d6df396b66331256
Ricardo Romo <rromom@est.ups.edu.ec>
2024-01-30 17:41:37 -0500

    [ADD][RR] Add fields and changes view

---
+++ trescloud_company_customizations/models/project_project.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, api, fields, models
+
+class ProjectProject(models.Model):
+    _inherit = 'project.project'
+
+    @api.depends('sale_line_id')
+    def _compute_order_id(self):
+        return False
+
+    localization = fields.Char(
+        string='Localización',
+        help='Ingrese la localización del proyecto',
+    )
+
+    order_id = fields.Many2one(
+        'sale.order',
+        string='Orden de Venta',
+        help='Numero de la orden de venta relacionado al proyecto',
+        compute='_compute_order_id'
+    )
