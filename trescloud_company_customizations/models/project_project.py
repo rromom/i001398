27a72ac538e13c807e1cbda7571d1b7b5b0dabb8
Ricardo Romo <rromom@est.ups.edu.ec>
2024-01-31 16:08:01 -0500

    [ADD][RR] Add comments

--- trescloud_company_customizations/models/project_project.py
+++ trescloud_company_customizations/models/project_project.py
@@ -14,14 +14,18 @@ class ProjectProject(models.Model):
             record.project_progress = record.last_update_id.progress
     
     def _compute_amount_sale_order(self):
+        '''
+        Calcula la sumatoria de las bases imponibles de todas las ordenes de ventas confirmadas asociadas
+        y la primera orden de venta que se confirmo y que se creo el proyecto
+        '''
         for record in self:
-            record.sale_order_task_id = False   
+            record.sale_order_task_id = False 
+            record.amount_sale_orders = 0.0
             if record.task_ids:
                 sales_orders = record.task_ids.mapped('sale_order_id').filtered(lambda order: order.state in ['sale', 'done'] )
                 if sales_orders:
                     record.amount_sale_orders = sum(sales_orders.mapped('amount_untaxed'))
                     record.sale_order_task_id = self.env['sale.order'].browse(min(sales_orders.mapped('id')))
-
 
     localization = fields.Char(
         string='Localización',
@@ -44,15 +48,14 @@ class ProjectProject(models.Model):
     sale_order_task_id = fields.Many2one(
         'sale.order',
         string='Numero de orden asociada',
+        help='Se selecciona la primera orden confirmada',
         compute='_compute_amount_sale_order',
-        default=False,
     )
 
     amount_sale_orders = fields.Float(
         string='Monto',
-        help='Monto total de las ordenes de venta',
+        help='Monto total de las ordenes de venta confirmadas',
         compute='_compute_amount_sale_order',
-        default=0.0,
     )
 
     
